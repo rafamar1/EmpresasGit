d1084a6aa14157657096bf849755fea9583467a3
rafamar1 <rafaelmarquezgonzalez92@gmail.com>
2017-02-13 19:46:19 +0100

    Planteamos la estructura del Main

--- src/empresasgit/EmpresasGit.java
+++ src/empresasgit/EmpresasGit.java
@@ -11,7 +11,7 @@ import utilidades67.ES.ES;
  */
 public class EmpresasGit {
     
-/*jajajajaj*/
+
     /**
      * @param args the command line arguments
      */
@@ -19,6 +19,50 @@ public class EmpresasGit {
     
     public static void main(String[] args) {
         
+        
+        listaEmpresas = new ArrayList();
+        int opcion;
+        do {
+            menu();
+
+            opcion = ES.leeN("Introduzca una opcion: ");
+
+            switch (opcion) {
+
+                case 1:
+                    listaEmpresas.add(new Empresa(pideCIF()));
+                    break;
+
+                case 2:
+                    altaPersonal('e');
+                    break;
+
+                case 3:
+                    altaPersonal('c');
+                    break;
+
+                case 4:
+                    int opcionDS = menuDirectivoySubordinado();
+                    manejaDirectivoSubordinado(opcionDS);
+                    break;
+
+                case 5:
+                    //manejaBajas();
+                    break;
+
+                case 6:
+                    listarEmpresas();
+                    System.out.println(listaEmpresas.get(pideNoEmpresa() - 1));
+                    break;
+
+                case 7:
+                    break;
+                default:
+                    System.out.println("¡¡Opcion Errónea!!");
+                    break;
+            }
+
+        } while (opcion != 7);
         
     }
     
